a518bbbbf713b118112b165d70b6217e2dc853d9
Suryadeep Pal <suryadeep10@gmail.com>
2026-09-06 16:41:33 +0530

    chore: use ConvoKit 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-    implementation("app.convokit:convokit-android:0.1.1")
+    implementation("app.convokit:convokit-android:0.2.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
